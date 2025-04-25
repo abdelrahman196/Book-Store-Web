15aa2bf5ec492fc7d5c28979abf7706311241be9
sofalotfy <lotfysofa@gmail.com>
2025-04-25 07:20:20 -0700

    k

--- booksPage/bookPage_script.js
+++ booksPage/bookPage_script.js
@@ -1,9 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    Const check_all= document.querySelector('.myCheckbox');
+    const check_all= document.querySelector('.myCheckbox');
     const sort_none= document.querySelector(".none_check");
+
+
+
+
+
+
+
+
     check_all.checked = true;
-
-
+    sort_none.checked = true;
+    const checks =document.querySelectorAll(".check");
+    const side_checks=document.querySelectorAll(".side_check");
+    for(let i=0;i<15;i++){
+    checks[i].addEventListener("click", function() {
+        side_checks[i].checked=true;
+        console.log(10)
+    } )
+}
 
   });
