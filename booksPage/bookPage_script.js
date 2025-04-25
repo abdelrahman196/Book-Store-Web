0ca654971dbfd9a7009c9c2823e0acb0f498daf7
sofalotfy <lotfysofa@gmail.com>
2025-04-25 13:54:15 -0700

    k

--- booksPage/bookPage_script.js
+++ booksPage/bookPage_script.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const check_all= document.querySelector('.myCheckbox');
     const sort_none= document.querySelector(".none_check");
 
-
-
+    const books_categories=document.querySelectorAll(`.category`);
+    const number_categorys=books_categories.length;
+    for(let i=0;i<length;i++){
+        if(books_categories[i].textContent==='none'){
+            books_categories[i].style.display='none';
+        }
+    }
 
 
 
@@ -14,10 +19,41 @@ document.addEventListener("DOMContentLoaded", function() {
     sort_none.checked = true;
     const checks =document.querySelectorAll(".check");
     const side_checks=document.querySelectorAll(".side_check");
-    for(let i=0;i<15;i++){
+    const checksA =  side_checks.length;
+    for(let i=0;i<checksA;i++){
     checks[i].addEventListener("click", function() {
-        side_checks[i].checked=true;
-        console.log(10)
+        if(i<4){
+            side_checks[i].checked=true;
+        }
+        else{
+            if(i===4){
+                if(!(side_checks[i].checked)){
+                    side_checks[i].checked=true;
+                    for(let x=i+1;x<checksA;x++){
+                        side_checks[x].checked=false;
+                    }
+                }
+            }
+            else{
+                if(side_checks[i].checked){
+                    let notAlone=false;
+                    for(let x=5;x<checksA;x++){
+                        if(side_checks[x].checked && x!==i){
+                            notAlone=true;
+                            break;
+                        }
+                    }
+                    side_checks[i].checked=false;
+                    if(!notAlone){
+                        side_checks[4].checked=true;
+                    }
+                }
+                else{
+                    side_checks[i].checked=true;
+                    side_checks[4].checked=false;
+                }
+            }
+        }
     } )
 }
 
