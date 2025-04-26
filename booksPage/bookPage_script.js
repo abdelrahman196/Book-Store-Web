07e903966cc1a942193030f1071b5140159320f9
sofalotfy <lotfysofa@gmail.com>
2025-04-25 18:43:08 -0700

    k

--- booksPage/bookPage_script.js
+++ booksPage/bookPage_script.js
@@ -2,7 +2,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const check_all= document.querySelector('.myCheckbox');
     const sort_none= document.querySelector(".none_check");
-
+    const books = document.querySelectorAll(`.book`);
+    const booksA = books.length;
     const books_categories=document.querySelectorAll(`.category`);
     const number_categorys=books_categories.length;
     for(let i=0;i<length;i++){
@@ -11,15 +12,50 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-
-
-
-
+    let len=0;
     check_all.checked = true;
     sort_none.checked = true;
     const checks =document.querySelectorAll(".check");
     const side_checks=document.querySelectorAll(".side_check");
     const checksA =  side_checks.length;
+    const checkText = document.querySelectorAll(`.check_text`);
+    const activeAll = function(){
+        for(let x=0;x<booksA;x++){
+            books[x].style.display='';
+        }
+    }
+    const activeCategory = function(name){
+        let len=0;
+        for(let x=0;x<booksA;x++){
+            let book_categories = books[x].querySelectorAll(`.category`);
+            let show=false;
+            for(let y=0;y<book_categories.length;y++){
+                if(name===books_categories[len+y].textContent){
+                    show=true;
+                }
+            }
+            if(show){
+                books[x].style.display=""
+            }
+            len+=book_categories.length;
+        }
+    }
+    const deactiveCategory = function(name){
+        let len=0;
+        for(let x=0;x<booksA;x++){
+            let book_categories = books[x].querySelectorAll(`.category`);
+            let show=false;
+            for(let y=0;y<book_categories.length;y++){
+                if(name===books_categories[len+y].textContent){
+                    show=true;
+                }
+            }
+            if(show){
+                books[x].style.display="none";
+            }
+            len+=book_categories.length;
+        }
+    }
     for(let i=0;i<checksA;i++){
     checks[i].addEventListener("click", function() {
         if(i<4){
@@ -32,6 +68,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     for(let x=i+1;x<checksA;x++){
                         side_checks[x].checked=false;
                     }
+                    activeAll();
                 }
             }
             else{
@@ -44,13 +81,24 @@ document.addEventListener("DOMContentLoaded", function() {
                         }
                     }
                     side_checks[i].checked=false;
+                    deactiveCategory(checkText[i].textContent);
+                    len=0
                     if(!notAlone){
                         side_checks[4].checked=true;
+                        activeAll()
                     }
                 }
                 else{
+                    if(side_checks[4].checked){
+                        for(let x=0;x<booksA;x++){
+                            books[x].style.display='none';
+                        }
+                    }
                     side_checks[i].checked=true;
                     side_checks[4].checked=false;
+
+                    activeCategory(checkText[i].textContent);
+                    len=0;
                 }
             }
         }
