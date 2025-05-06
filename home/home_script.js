55c021931b577123c4553e6b1c65b72c7d2f82b3
Abdallah10197 <334510197Aa@gmail.com>
2025-05-06 17:48:46 +0300

    k

--- home/home_script.js
+++ home/home_script.js
@@ -4,3 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
           window.location.href = "./profile.html"; 
   })
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const home = document.getElementById("nav__item1");
+      home.addEventListener("click", function() {
+          window.location.href = "./home.html"; 
+  })
+});
+
