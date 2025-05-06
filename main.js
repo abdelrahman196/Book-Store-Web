a314796086776c19491431d3de9e6f61ee889471
Abdallah10197 <334510197Aa@gmail.com>
2025-05-06 14:37:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const logo = document.getElementById("logo");
+      logo.addEventListener("click", function() {
+          window.location.href = "./file.html"; 
+        })
+    });
+
+
+
+document.querySelector('.signout-btn').addEventListener('click', () => {
+    alert('Signed out successfully!');
+});
+
+document.querySelector('.delete-btn').addEventListener('click', () => {
+    if (confirm('Are you sure you want to delete your account?')) {
+    alert('Account deleted.');
+        // actual deletion logic here
+    }
+});
+
+document.querySelector('.history-btn').addEventListener('click', () => {
+    const container = document.querySelector('.history-table-container');
+    container.style.display = container.style.display === 'none' ? 'block' : 'none';
+});
+
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.left = sidebar.style.left === '0px' ? '-220px' : '0px';
+});
