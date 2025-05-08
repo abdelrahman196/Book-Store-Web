625a7e83f0b8d24eea8110dfce32a76becb59207
mariam99494 <mariamtarekosmann@gmail.com>
2025-05-08 22:21:57 +0300

    nn

--- JS/file_script.js
+++ JS/file_script.js
@@ -4,3 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
           window.location.href = "./index.html"; 
   })
 });
+
+// Theme Button
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('active');
+  const icon = btn.querySelector('.btn__icon');
+  icon.classList.add('animated');
+  // Remove animation class after it completes
+  setTimeout(() => {
+    icon.classList.remove('animated');
+  }, 500);
+});  
