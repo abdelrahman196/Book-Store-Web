66e8aa2b798a370c84b1c7c5110dd7efbf09f8bd
sofalotfy <lotfysofa@gmail.com>
2025-05-10 08:31:40 -0700

    k

--- vibe.js
+++ vibe.js
@@ -1,34 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {    
 const rate_buts = document.querySelectorAll(`.rating`)
+const list =document.querySelectorAll(`.rating_list`)
+for(let i=0;i<3;i++){
+    list[i].style.display="none";
+}
 for(let i=0;i<3;i++){
     rate_buts[i].addEventListener("click", function(){
-        const list =this.querySelector(`.rating_list`)
-        if(list.display==="block"){
-            list.display="none";
+        if(list[i].style.display=="block"){
+            console.log(9)
+            list[i].style.display="none";
             rate_buts[i].textContent="Rate >";
         }
         else{
-            list.display="block";
+            console.log(7)
+            list[i].style.display="block";
             rate_buts[i].textContent="Rate v";
         }
     })
 }
-const rate_op = document.querySelectorAll(`.rate`)
+const rate = document.querySelectorAll(`.rate`)
 for(let i = 0;i<5;i++){
     rate[i].addEventListener("click",function(){
         rate_buts[0].textContent=rate[i].textContent;
-        rate_buts[0].querySelector(`.rating_list`).display="none";
+        rate_buts[0].querySelector(`.rating_list`).style.display="none";
     })
 }
 for(let i = 5;i<10;i++){
     rate[i].addEventListener("click",function(){
         rate_buts[1].textContent=rate[i].textContent;
-        rate_buts[1].querySelector(`.rating_list`).display="none";
+        rate_buts[1].querySelector(`.rating_list`).style.display="none";
     })
 }
 for(let i = 10;i<15;i++){
     rate[i].addEventListener("click",function(){
         rate_buts[2].textContent=rate[i].textContent;
-        rate_buts[2].querySelector(`.rating_list`).display="none";
+        rate_buts[2].querySelector(`.rating_list`).style.display="none";
     })
 }
 const sub = document.querySelector(`.sub`).addEventListener("click",function(){
@@ -36,3 +42,4 @@ const sub = document.querySelector(`.sub`).addEventListener("click",function(){
         rate_buts[i].textContent="rate >";
     }
 })
+});
