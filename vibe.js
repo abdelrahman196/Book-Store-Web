329963a119a36d60766a419da2e7de157288c777
sofalotfy <lotfysofa@gmail.com>
2025-05-10 09:02:15 -0700

    k

--- vibe.js
+++ vibe.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {    
 const rate_buts = document.querySelectorAll(`.rating`)
 const list =document.querySelectorAll(`.rating_list`)
-for(let i=0;i<3;i++){
-    list[i].style.display="none";
-}
+
 for(let i=0;i<3;i++){
     rate_buts[i].addEventListener("click", function(){
         if(list[i].style.display=="block"){
-            console.log(9)
             list[i].style.display="none";
             rate_buts[i].textContent="Rate >";
         }
         else{
-            console.log(7)
             list[i].style.display="block";
             rate_buts[i].textContent="Rate v";
         }
@@ -22,19 +18,19 @@ const rate = document.querySelectorAll(`.rate`)
 for(let i = 0;i<5;i++){
     rate[i].addEventListener("click",function(){
         rate_buts[0].textContent=rate[i].textContent;
-        rate_buts[0].querySelector(`.rating_list`).style.display="none";
+        list[0].style.display="none";
     })
 }
 for(let i = 5;i<10;i++){
     rate[i].addEventListener("click",function(){
         rate_buts[1].textContent=rate[i].textContent;
-        rate_buts[1].querySelector(`.rating_list`).style.display="none";
+        list[1].style.display="none";
     })
 }
 for(let i = 10;i<15;i++){
     rate[i].addEventListener("click",function(){
         rate_buts[2].textContent=rate[i].textContent;
-        rate_buts[2].querySelector(`.rating_list`).style.display="none";
+        list[2].style.display="none";
     })
 }
 const sub = document.querySelector(`.sub`).addEventListener("click",function(){
@@ -42,4 +38,26 @@ const sub = document.querySelector(`.sub`).addEventListener("click",function(){
         rate_buts[i].textContent="rate >";
     }
 })
+const coms = document.querySelectorAll(".pop");
+const pops = document.querySelectorAll(".pop_up");
+for(let i =0;i<3;i++){
+    coms[i].addEventListener("click",function(){
+        (pops[i].style.display==="none")?pops[i].style.display="block":pops[i].style.display="none";
+    })
+}
+const popsubs = document.querySelectorAll(`.pop-sub`);
+const popheads = document.querySelectorAll(`.pop-header`)
+const popbodys = document.querySelectorAll(`.pop-body-big`)
+for(let i = 0 ;i<3;i++){
+    popsubs[i].addEventListener("click", function(){
+        popheads[i].value ="";
+        if(i==0){
+            document.querySelector(`.pop-body`).value="";
+        }
+        else{
+            popbodys[i-1].value="";
+        }
+        pops[i].style.display="none";
+    })
+}
 });
