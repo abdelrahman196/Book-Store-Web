7eadc993d6c81d7b1db955fe94269cf46000fc59
Lama Wael <lamawael35@gmail.com>
2025-05-10 21:05:30 +0300

    Update 2nd.js
    changed the arrow function to the regular one cuz no difference here

--- JS/2nd.js
+++ JS/2nd.js
@@ -2,17 +2,17 @@ document.getElementById("logo").addEventListener("click",function(){
     window.location.href="./file.html";});
 
 
-document.querySelector('.signout-btn').addEventListener('click', () => {
+document.querySelector('.signout-btn').addEventListener('click',function () {
     alert('Signed out successfully!');
 });
 
-document.querySelector('.delete-btn').addEventListener('click', () => {
+document.querySelector('.delete-btn').addEventListener('click',function ()  {
     if (confirm('Are you sure you want to delete your account?')) {
     alert('Account deleted.');
     }
 });
 
-document.querySelector('.history-btn').addEventListener('click', () => {
+document.querySelector('.history-btn').addEventListener('click',function () {
     const container = document.querySelector('.history-table-container');
     container.style.display = container.style.display === 'none' ? 'block' : 'none';
 });
